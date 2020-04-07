0e9e7d1b9d41712078d8ef0fa53f33a1c6454671
Sethu Ganapathi <SGanapathi@geico.com>
2020-04-06 21:46:28 -0400

    Add project files.

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDb = new MusicStoreEntities();
+
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var genres = storeDb.Genres.ToList();
+            return View(genres);
+        }
+        //
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            // Retrieve Genre and its Associated Albums from database
+            var genreModel = this.storeDb.Genres.Include("Albums").Single(g => g.Name == genre); ;
+
+            return View(genreModel);
+        }
+        //
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var store = this.storeDb.Albums;
+            var album = this.storeDb.Albums.Find(id);
+
+            return View(album);
+        }
+    }
+}
---
+++ WebApiTokenAuthentication/MyAuthorizationServerProvider.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.OAuth;
+
+namespace WebApiTokenAuthentication
+{
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated(); 
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Sethu"));
+                context.Validated(identity);
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Yaalu"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("Invalid grant", "Provided user name and pasword is incorrect");
+            }
+
+            return base.GrantResourceOwnerCredentials(context);
+        }
+    }
+}
